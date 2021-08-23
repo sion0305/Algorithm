df6dfe0f5c64add0b364e25074c4c39385218cff
sion <aabbcc030421@gamil.com>
2021-08-23 21:33:46 +0900

    DP *6

--- Algorithm/src/baekjoon/Main_9461.java
+++ Algorithm/src/baekjoon/Main_9461.java
@@ -2,30 +2,31 @@ package baekjoon;
 
 import java.util.Scanner;
 
-public class Main_9095 {
-	static int[] dp = new int[11];
+public class Main_9461 {
+	static long[] dp = new long[101];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int t = sc.nextInt();
 		
+		dp[0] = 0;
 		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 4;
+		dp[2] = 1;
+		dp[3] = 1;
 		
 		for(int i = 0; i < t; i++) {
 			int n = sc.nextInt();
 			
-			System.out.println(dp(n));
+			System.out.println(solution(n));
 		}
-
 	}
 	
-	public static int dp(int n) {
-		if(dp[n] != 0)
+	public static long solution(int n) {
+		if(dp[n] != 0) {
 			return dp[n];
+		}
 		
-		return dp[n] = dp(n-1) + dp(n-2) + dp(n-3);
+		return dp[n] = solution(n-2) + solution(n-3);
 	}
 
 }
