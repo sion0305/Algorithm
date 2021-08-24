40db9304598a8d1f4bbefe8cfe9367e7b7690413
sion <aabbcc030421@gmail.com>
2021-08-24 17:34:58 +0900

    2021 kakao2

---
+++ Algorithm/src/Programmers/문자열_압축_0.java
@@ -0,0 +1,58 @@
+package Programmers;
+
+public class 문자열_압축_0 {// 2021 kakao
+
+	// self -> 실패(42.0) -> 성공
+	public static void main(String[] args) {
+//		String s = "aabbaccc";
+//		String s = "ababcdcdababcdcd";
+		String s = "abcabcdede";
+
+		System.out.println(solution(s));
+
+	}
+
+	public static int solution(String s) {
+		int answer = s.length();
+
+		int size = s.length();
+
+		String cut1 = s, cut2 = s;
+		for (int i = 1; i <= size; i++) {
+			cut1 = s.substring(0, i);
+			int count = 1;
+			StringBuffer comp = new StringBuffer();
+			for (int j = i; j < size; j += i) {
+				//수정
+				if(j + i > size) {
+					cut2 = s.substring(j);
+				}else {
+					cut2 = s.substring(j, j + i);
+				}
+				//수정
+				
+				if (cut1.equals(cut2)) {
+					count++;
+				} else {
+					if (count > 1) {
+						comp.append(count + cut1);
+					} else {
+						comp.append(cut1);
+					}
+
+					cut1 = cut2;
+					count = 1;
+				}
+			}
+			if (count > 1) {
+				comp.append(count + cut1);
+			} else {
+				comp.append(cut1);
+			}
+
+			answer = Math.min(answer, comp.length());
+		}
+
+		return answer;
+	}
+}
